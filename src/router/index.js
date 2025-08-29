a0990fd696597d4d0e2a766fc6b08caae1fe99f2
28448812 <zche0189@student.monash.edu>
2025-08-29 19:29:55 +1000

    Add homepage and login page

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+
+import AppLayout from '@/layout/AppLayout.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: AppLayout,
+            children: [
+                {
+                    path: '/',
+                    name: 'dashboard',
+                    component: () => import('@/views/Dashboard.vue')
+                },
+            ]
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: () => import('@/views/home.vue')
+        },
+       
+        {
+            path: '/landing',
+            name: 'landing',
+            component: () => import('@/views/pages/Landing.vue')
+        },
+        {
+            path: '/auth/login',
+            name: 'login',
+            component: () => import('@/views/auth/Login.vue')
+        }
+    ]
+});
+
+export default router;
